fa95d282bf0d1f4c74f738ae1b239339b6ba7f4b
pjohans <pjohans@gmail.com>
2015-10-14 12:45:13 +0200

    added files and classes for drupal integration

---
+++ includes/TingClientDrupalCacher.php
@@ -0,0 +1,15 @@
+<?php
+class TingClientDrupalCacher implements ITingClientCacherInterface{
+  function set($key, $value) {
+    // TODO: Implement set() method.
+  }
+
+  function get($key) {
+    // TODO: Implement get() method.
+  }
+
+  function clear() {
+    // TODO: Implement clear() method.
+  }
+
+}
---
+++ includes/ting_client_class.php
@@ -0,0 +1,7 @@
+<?php
+
+class ting_client_class extends TingClientBaseClass{
+  public function do_request($name, $params){
+    return parent::do_request($name, $params);
+  }
+}
