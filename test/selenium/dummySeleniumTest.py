19dafe17452af06fe111580f624718a8c2d2a4fa
JrgenGNielsen <jgn@dbc.dk>
2015-10-15 15:00:12 +0200

    develop jenkins selenium tests

--- test/selenium/dummySeleniumTest.py
+++ test/selenium/dummySeleniumTest.py
@@ -5,11 +5,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 build_url = os.getenv('FEATURE_BUILD_URL', '')
-print 'build_url: ', build_url
+print 'Selenium test URL: ', build_url
 
 driver = webdriver.PhantomJS()
 driver.get(build_url)
 assert "Site-Install" in driver.title
-print 'driver.title: ', driver.title
 
 driver.close()
