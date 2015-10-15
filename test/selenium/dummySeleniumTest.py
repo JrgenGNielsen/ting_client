aee2391444e0624a1c704e21e1639a52432e39b6
JrgenGNielsen <jgn@dbc.dk>
2015-10-15 12:36:18 +0200

    debugging jenkins selenium tests

--- test/selenium/dummySeleniumTest.py
+++ test/selenium/dummySeleniumTest.py
@@ -4,6 +4,9 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+build_url = os.getenv('BUILD_URL', None)
+print 'build_url: ', build_url
+
 driver = webdriver.PhantomJS()
 driver.get("http://www.python.org")
 # assert "Foo" in driver.title
