33dbd45c98571869501469fa8054a65a007a3004
JrgenGNielsen <jgn@dbc.dk>
2015-10-15 14:30:28 +0200

    develop jenkins selenium tests

--- test/selenium/dummySeleniumTest.py
+++ test/selenium/dummySeleniumTest.py
@@ -4,7 +4,7 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-build_url = os.getenv('FEATURE_BUILD_URL', None)
+build_url = os.getenv('FEATURE_BUILD_URL', '')
 print 'build_url: ', build_url
 
 driver = webdriver.PhantomJS()
