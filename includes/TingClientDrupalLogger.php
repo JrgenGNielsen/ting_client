68e1bebd2b74414cc27b4f42990ad19bbf112250
pjohans <pjohans@gmail.com>
2015-11-10 10:58:49 +0100

    better logging

--- includes/TingClientDrupalLogger.php
+++ includes/TingClientDrupalLogger.php
@@ -8,7 +8,26 @@
  * Basically class wraps watchdog.
  */
 class TingClientDrupalLogger extends TingClientLogger{
-  protected function doLog($message, $variables, $severity) {
-    watchdog('ting client', $message, $variables, $severity);
+  public function doLog($message_type, $variables, $severity) {
+    $variables['time'] = $this->log_time;
+    $vars = array();
+    foreach ($variables as $key => $value){
+      $vars['@'.$key] = $value;
+    }
+    switch($message_type){
+      case 'soap_request_complete':
+        $message = 'Completed SOAP request @action @wsdlUrl ( @time s). Request body: @requestBody';
+        break;
+      case 'soap_request_error':
+        $message = 'Error handling SOAP request @action @wsdlUrl: @error';
+        break;
+      default :
+        $vars['@type'] = $message_type;
+        $message = '@type request @action @wsdlUrl ( @time s). Request body: @requestBody';
+    }
+
+    watchdog('ting client',$message, $variables,
+      constant('WATCHDOG_' . $severity),
+      'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
   }
 }
